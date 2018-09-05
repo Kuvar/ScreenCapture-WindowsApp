1cd0cbd036a90f43ab12567f17a3203fc839f9d9
Kuvar Singh <kuvarjava@gmail.com>
2018-09-05 10:16:54 +0530

    Windows Forms Application
    Windows Forms Application

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(975, 464);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(2, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(975, 25);
+            this.panel1.TabIndex = 4;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ForeColor = System.Drawing.Color.Transparent;
+            this.radioButton2.Location = new System.Drawing.Point(106, 3);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.Text = "SaveInWord";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.ForeColor = System.Drawing.Color.Transparent;
+            this.radioButton1.Location = new System.Drawing.Point(13, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(79, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "SaveImage";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 497);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CaptureScreen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using ScreenShotDemo;
+using Xceed.Words.NET;
+using Image = Xceed.Words.NET.Image;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        int counter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            RegisterHotKeyClass registerHotKey=new RegisterHotKeyClass();
+            registerHotKey.Keys = Keys.PrintScreen;
+            registerHotKey.ModKey = 0;
+            registerHotKey.WindowHandle = this.Handle;
+            registerHotKey.HotKey += new RegisterHotKeyClass.HotKeyPass(registerHotKeyHandler);
+            registerHotKey.StarHotKey();
+        }
+
+        void registerHotKeyHandler()
+        {
+            if (radioButton1.Checked)
+            {
+                SaveToImage();
+            }
+            else if (radioButton2.Checked)
+            {
+                SaveToWordFile();
+            }
+            else
+            {
+                MessageBox.Show("Please Select Source Type","InforMation",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+        }
+
+        public void SaveToImage()
+        {
+            try
+            {
+                string fileName = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\" + "Capture Screen" + "\\" + "Image" + "\\" + DateTime.Now.ToString("dd-MM-yyyy") + "\\";
+                if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\" + "Capture Screen" + "\\" + "Image" + "\\" + DateTime.Now.ToString("dd-MM-yyyy") + "\\"))
+                {
+                    Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\" + "Capture Screen" + "\\" + "Image" + "\\" + DateTime.Now.ToString("dd-MM-yyyy") + "\\");
+                }
+                ScreenCapture sc = new ScreenCapture();
+                // capture entire screen, and save it to a file
+                System.Drawing.Image img = sc.CaptureScreen();
+                // display image in a Picture control named imageDisplay
+                pictureBox1.Image = img;
+                // capture this window, and save it;
+                //sc.CaptureWindowToFile(this.Handle, Guid.NewGuid()+".gif", ImageFormat.Gif);
+                img.Save(fileName + "\\" + "Screenshot" + Guid.NewGuid() + ".jpg", ImageFormat.Gif);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void SaveToWordFile()
+        {
+            ScreenCapture sc = new ScreenCapture();
+            // capture entire screen, and save it to a file
+            System.Drawing.Image img1 = sc.CaptureScreen();
+            // display image in a Picture control named imageDisplay
+            pictureBox1.Image = img1;
+            // capture this window, and save it;
+            //sc.CaptureWindowToFile(this.Handle, Guid.NewGuid()+".gif", ImageFormat.Gif);
+            img1.Save("Screenshot" + ".jpg", ImageFormat.Gif);
+            counter++;
+            string fileName = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\" + "Capture Screen" + "\\";
+            if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\" + "Capture Screen" + "\\"))
+            {
+                Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\" + "Capture Screen" + "\\");
+            }
+            if (!File.Exists(fileName + DateTime.Now.ToString("dd-MM-yyyy") + ".docx"))
+            {
+                var docCreate = DocX.Create(fileName + DateTime.Now.ToString("dd-MM-yyyy") + ".docx");
+                docCreate.Save();
+            }
+            var docload = DocX.Load(fileName + DateTime.Now.ToString("dd-MM-yyyy") + ".docx");
+            Image img = docload.AddImage("Screenshot.jpg");
+            Picture p = img.CreatePicture();
+            p.Width = 690;
+            p.Height = 365;
+            Xceed.Words.NET.Paragraph par = docload.InsertParagraph(Convert.ToString(counter));
+            par.AppendPicture(p);
+            docload.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+        }
+    }
+}
